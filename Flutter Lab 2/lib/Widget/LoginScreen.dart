451281786ef05232be627a16316d2ebccc5902de
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-09-30 14:26:17 +0500

    Damn girl

--- Flutter Lab 2/lib/Widget/LoginScreen.dart
+++ Flutter Lab 2/lib/Widget/LoginScreen.dart
@@ -11,11 +11,27 @@ class LoginScreen extends StatelessWidget
     (
       appBar: AppBar
       (
+        leading: const Icon(Icons.add),
         title: const Text("GDSC Pocket App"),
         backgroundColor: Color.fromARGB(255, 66, 133, 244),
-        titleTextStyle: TextStyle
+        titleTextStyle: const TextStyle
         (
-          color: Colors.white
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+      body: const Padding
+      (
+        padding: EdgeInsets.all(10.0),
+        child: Column
+        (
+          children:
+          [
+            TextField
+            (
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );

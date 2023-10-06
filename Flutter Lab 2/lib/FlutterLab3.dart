e5d7f6c29db0b50525364b81dc8523a5066acb92
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-06 09:22:15 +0500

    CodeLab1 done

--- Flutter Lab 2/lib/FlutterLab3.dart
+++ Flutter Lab 2/lib/FlutterLab3.dart
@@ -1,13 +1,21 @@
 // Mid mein UI, API integration, widget
 // Tomorrow quiz and class activity
 // Codelabs is in quiz tomorrow
-
-
 import 'package:flutter/material.dart';
 
-class CodeLab1 extends StatelessWidget
+
+class CodeLab1 extends StatefulWidget
 {
-  const CodeLab1({super.key});
+  const CodeLab1({Key? key}) : super(key: key);
+
+  @override
+  _CodeLab1State createState() => _CodeLab1State();
+}
+
+class _CodeLab1State extends State<CodeLab1> 
+{
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context)
@@ -16,7 +24,7 @@ class CodeLab1 extends StatelessWidget
     (
       body: SafeArea
       (
-        child: ListView
+        child: ListView // We use ListView here and not Column because Column does not handle scrolling and we want that because we will open keyboard to type in this field
         (
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>
@@ -39,6 +47,7 @@ class CodeLab1 extends StatelessWidget
             const SizedBox(height: 120.0),
             TextField
             (
+              controller: _usernameController,
               decoration: const InputDecoration
               (
                 filled: true,
@@ -48,12 +57,47 @@ class CodeLab1 extends StatelessWidget
             const SizedBox(height: 12.0),
             TextField
             (
+              controller: _passwordController,
               decoration: const InputDecoration
               (
                 filled: true,
                 labelText: 'Password',
               ),
               obscureText: true,
+            ),
+            const SizedBox(height: 50.0),
+            OverflowBar
+            (
+              alignment: MainAxisAlignment.center,
+              overflowDirection: VerticalDirection.down,
+              children:
+              [
+                Column
+                (
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  
+                  children: <Widget>
+                  [
+                    ElevatedButton
+                    (
+                      onPressed: ()
+                      {
+                        _passwordController.clear();
+                        Navigator.pop(context);
+                      },
+                      child: const Text('NEXT'),
+                    ),
+                    TextButton
+                    (
+                      onPressed: ()
+                      {
+                        _usernameController.clear();
+                      },
+                      child: const Text('CANCEL'),
+                    ),
+                  ],
+                ),
+              ]
             ),
           ]
         ),

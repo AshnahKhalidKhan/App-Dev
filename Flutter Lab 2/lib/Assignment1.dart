8e98ab9bb094323d26ed00cf18e8835f917e449b
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-07 11:35:42 +0500

    Math be mathing

--- Flutter Lab 2/lib/Assignment1.dart
+++ Flutter Lab 2/lib/Assignment1.dart
@@ -1,208 +1,3 @@
-// import 'dart:convert';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_1/main.dart';
-// import 'package:http/http.dart' as http;
-
-// //Async and future are important keywords because we don't return data until it has been received and we don't know when it will be received
-// // Future <List<Comments>> fetchComments() async
-// // {
-// //   final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
-// //   // print(response.body.toString());
-// //   if ((response.statusCode == 200))
-// //   {
-// //     List<dynamic> _parsedListJson = jsonDecode(response.body);
-// //     List<Comments> _itemsList = List<Comments>.from
-// //     (
-// //       _parsedListJson.map<Comments>
-// //       (
-// //         (dynamic i) => Comments.fromJson(i)
-// //       )
-// //     );
-// //     return _itemsList;
-// //   }
-// //   else
-// //   {
-// //     throw Exception('Failed to load Comments');
-// //   }
-// // }
-
-// // //Json Model Class
-// // class Comments
-// // {
-// //   final int userId;
-// //   final int id;
-// //   final String title;
-
-// //   const Comments
-// //   (
-// //     {
-// //       required this.userId,
-// //       required this.id,
-// //       required this.title
-// //     }
-// //   );
-
-// //   //Whatever API reposonse you get,
-// //   factory Comments.fromJson(Map<String, dynamic> json)
-// //   {
-// //     return Comments
-// //     (
-// //       userId: json['userId'],
-// //       id: json['id'],
-// //       title: json['title']
-// //     );
-// //   }
-// // }
-
-// // void main() => runApp(const MyApp());
-
-// // class KoiBhiStatefulWidget extends StatefulWidget
-// // {
-// //   const KoiBhiStatefulWidget({super.key});
-
-// //   @override
-// //   State<KoiBhiStatefulWidget> createState() => _statefulWidgetKiState();
-// // }
-
-// // class _statefulWidgetKiState extends State<KoiBhiStatefulWidget>
-// // {
-// //   @override
-// //   Widget build(BuildContext context)
-// //   {
-// //     return Scaffold
-// //     (
-// //       appBar: AppBar
-// //       (
-// //         backgroundColor: Colors.red,
-// //       ),
-// //       body: Center
-// //       (
-// //         child: FutureBuilder<List<Comments>>
-// //         (
-// //           future: fetchComments(),
-// //           builder: (context, snapshot)
-// //           {
-// //             if (snapshot.hasData)
-// //             {
-// //               return ListView.builder
-// //               (
-// //                 itemCount: snapshot.data?.length,
-// //                 itemBuilder: (context, i)
-// //                 {
-// //                   var item = snapshot.data![i];
-// //                   // return Text(item.title);
-// //                   return Card
-// //                   (
-// //                     shape: RoundedRectangleBorder
-// //                     (
-// //                       borderRadius: BorderRadius.circular(8.0),
-// //                     ),
-// //                     color: Colors.blue,
-// //                     child: ListTile
-// //                     (
-// //                       leading: const Icon
-// //                       (
-// //                         Icons.add_circle_outline_rounded,
-// //                         size: 50,
-// //                         color: Colors.white,
-// //                       ),
-// //                       title: Text
-// //                       (
-// //                         item.title,
-// //                         overflow: TextOverflow.visible,
-// //                         textAlign: TextAlign.center,
-// //                         style: const TextStyle
-// //                         (
-// //                           color: Colors.white,
-// //                           fontSize: 20,
-// //                           overflow: TextOverflow.visible,
-// //                         ),
-// //                       ),
-// //                     ),
-// //                   );
-// //                 },
-// //               );
-// //             }
-// //             else if (snapshot.hasError)
-// //             {
-// //               return Text('${snapshot.error}');
-// //             }
-// //             else
-// //             {
-// //               return const CircularProgressIndicator();
-// //             }
-// //           }
-// //         )
-// //       ),
-// //     );
-// //   }
-
-// //   @override
-// //   void initState()
-// //   {
-// //     super.initState();
-// //     fetchComments();
-// //   }
-// // }
-
-// class Comments
-// {
-//   // "postId": 1,
-//   //       "id": 1,
-//   //       "name": "id labore ex et quam laborum",
-//   //       "email": "Eliseo@gardner.biz",
-//   //       "body":
-//   final int postId;
-//   final int id;
-//   final String name;
-//   final String email;
-//   final String body;
-
-//   const Comments
-//   {
-//     {
-//       required this.postId,
-//       required this.id,
-//       required this.name,
-//       required this.email,
-//       required this.body
-//     }
-
-//     factory Comments.fromJson(Map<String, dynamic> json)
-//     {
-//       return Comments
-//       (
-//         postId: json['postId'],
-//         id: json['id'],
-//         name: json['name'],
-//         email: json['email'],
-//         body: json['body']
-//       );
-//     }
-//   }
-// }
-
-// void main() => runApp(const MyApp());
-
-// class Assignment1KiStatefulWidget extends StatefulWidget
-// {
-//   const Assignment1KiStatefulWidget({super.key});
-
-//   @override
-//   State<Assignment1KiStatefulWidget> createState() => _Assignment1KiStatefulWidgetState();
-// }
-
-// class _Assignment1KiStatefulWidgetState extends State<Assignment1KiStatefulWidget>
-// {
-//   @override
-//   Widget build(BuildContext context)
-//   {
-//     return Scaffold
-//     (
-//       appBar: AppBar
-//       (
-//         backgroundColor: Colors.blue,
-//       ),
 //       body: Center
 //       (
 //         child: FutureBuilder<List<Comments>>
@@ -266,34 +61,10 @@
 //   }
 // }
 
-// //Async and future are important keywords because we don't return data until it has been received and we don't know when it will be received
-// Future <List<Comments>> fetchComments() async
-// {
-//   final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/comments'));
-//   if ((response.statusCode == 200))
-//   {
-//     List<dynamic> _parsedListJson = jsonDecode(response.body);
-//     List<Comments> _itemsList = List<Comments>.from
-//     (
-//       _parsedListJson.map<Comments>
-//       (
-//         (dynamic i) => Comments.fromJson(i)
-//       )
-//     );
-//     return _itemsList;
-//   }
-//   else
-//   {
-//     throw Exception('Failed to load Comments');
-//   }
-// }
-
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main.dart';
-// import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Comments
 {
@@ -349,13 +120,71 @@ class _Assignment1State extends State<Assignment1>
         backgroundColor: Colors.blue,
         title: const Text("Assignment 1"),
       ),
+      body: Center
+      (
+        child: FutureBuilder
+        (
+          future: fetchComments(),
+          builder: (context, snapashot)
+          {
+            if (snapashot.hasData)
+            {
+              return GridView.builder
+              (
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (context, int i)
+                {
+                  return GridTile
+                  (
+                    child: Card
+                    (
+                      child: Column
+                      (
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>
+                        [
+                          Container
+                          (
+                            alignment: Alignment.center,
+                            width: 50,
+                            height: 50,
+                            decoration: const BoxDecoration
+                            (
+                              shape: BoxShape.circle,
+                              color: Colors.blue,
+                            ),
+                            child: Text
+                            (
+                              snapashot.data![i].id.toString()
+                            ),
+                          ),
+                          Text(snapashot.data![i].name),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              );
+            }
+            else if (snapashot.hasError)
+            {
+              return Text('$snapashot.error');
+            }
+            else
+            {
+              return CircularProgressIndicator();
+            }
+          }
+        ),
+      ),
     );
   }
 }
 
 Future<List<Comments>> fetchComments() async
 {
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com//comments'));
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/comments'));
   if (response.statusCode == 200)
   {
     List<dynamic> _parsedList = jsonDecode(response.body);
@@ -363,6 +192,7 @@ Future<List<Comments>> fetchComments() async
     (
       _parsedList.map<Comments>((dynamic i) => Comments.fromJson(i))
     );
+    // print(_itemsList.toString());
     return _itemsList;
   }
   else
